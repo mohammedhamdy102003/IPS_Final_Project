85a5457b2a6baf22637a02ed14bee167511e67c4
mohammed hamdy <mohammedhamdy102003@gmail.com>
2026-06-20 22:56:47 +0300

    Add missing migration

---
+++ Migrations/20260620195548_FixModelChanges.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IPS_PROJECT.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixModelChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TrafficType",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Protocol",
+                table: "AlertNotifications");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TrafficType",
+                table: "Events",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Protocol",
+                table: "AlertNotifications",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
